af2e3bed18f8a0a477ffcc879fb397fac67b0cba
Carlos de la Torre <cmdelatorre@gmail.com>
2019-03-04 12:49:06 -0300

    Initialize the Reservations app

---
+++ reservations/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import models
+
+
+class Reservation(models.Model):
+    from_date = models.DateField("desde")
+    to_date = models.DateField("hasta")
+    rooms = models.ManyToManyField(
+        "rooms.room", verbose_name="Habitación reservada", related_name="reservas"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="persona",
+        on_delete=models.CASCADE,
+        related_name="reservas",
+    )
+    notas = models.TextField("notas", blank=True, default="")
+
+    def __str__(self):
+        return "Reserva de {r.user} ({r.from_date} al {r.to_date})".format(r=self)
+
+    class Meta:
+        ordering = ["from_date", "to_date"]
+        verbose_name = "Reserva"
+        verbose_name_plural = "Reservas"
