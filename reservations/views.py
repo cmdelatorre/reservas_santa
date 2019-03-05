fc3542f8192f19fdf433e744ff7e9959c6a46249
Carlos de la Torre <cmdelatorre@gmail.com>
2019-03-05 12:28:54 -0300

    Start with basic list view

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from datetime import date
 
-# Create your views here.
+from reservations.models import Reservation
+
+
+def index(request):
+    template = loader.get_template("reservations/index.html")
+    today = date.today()
+    context = {
+        "next": Reservation.objects.filter(from_date__gte=today),
+        "past": Reservation.objects.filter(from_date__lt=today),
+    }
+    return HttpResponse(template.render(context, request))
