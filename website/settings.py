1b6e8eb54f8c9c4c034bc93482fb62185b2da0bd
Carlos de la Torre <cmdelatorre@gmail.com>
2019-03-17 12:11:52 -0300

    config templateing

--- website/settings.py
+++ website/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "website.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["global_templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
