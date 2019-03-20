e2dbb8064807a3fd3e41c9db634c6021b4b662f1
Carlos de la Torre <cmdelatorre@gmail.com>
2019-03-20 12:08:10 -0300

    Improved styles and links

--- website/settings.py
+++ website/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # https://django-registration.readthedocs.io/en/3.0/quickstart.html
 
